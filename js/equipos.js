2e593e8eaaa35da3a76e89d5402bbfc7e8c21e0a
jetzz-uwux <jetzaly2006@gmail.com>
2026-06-07 15:20:56 -0600

    funcion en filtrado agregado en todas las paginas

--- js/equipos.js
+++ js/equipos.js
@@ -5,24 +5,71 @@ xhttp.onload = function() {cargarTabla(this)}
 xhttp.open("GET", "xml/equipos.xml");
 xhttp.send();
 
+let todosEquipos = [];
 function cargarTabla(xml){
     const xmlDoc = xml.responseXML;
-    const p = xmlDoc.getElementsByTagName("equipo")
+    const e = xmlDoc.getElementsByTagName("equipo")
     let filas =""
-    for (let i = 0; i <p.length; i++) {
-         const miembros = p[i].getElementsByTagName("miembro");
+    for (let i = 0; i <e.length; i++) {
+         const miembros = e[i].getElementsByTagName("miembro");
         let listaMiembros = "";
         for (let j = 0; j < miembros.length; j++) {
             listaMiembros += miembros[j].childNodes[0].nodeValue + " (" + miembros[j].getAttribute("rol") + ") ";
         }
+        todosEquipos.push({
+            nombre: e[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue,
+            tipo: e[i].getAttribute("tipo"),
+            reaccion: e[i].getAttribute("reaccion"),
+            personajes:listaMiembros,
+            descripcion:e[i].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue
+        })
+
         filas += "<tr><td>" +
-        p[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue +
+        e[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue +
         "</td><td>" +
-        p[i].getAttribute("tipo") + "</td><td>" +
-        p[i].getAttribute("reaccion") +
+        e[i].getAttribute("tipo") + "</td><td>" +
+        e[i].getAttribute("reaccion") +
         "</td><td>" +
-        listaMiembros +"</td><td>"+p[i].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue +
+        listaMiembros +"</td><td>"+
+        e[i].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue +
         "</td></tr>"
+
+        
     }
     tabla.innerHTML = filas;
+}
+
+
+document.getElementById('tipo').addEventListener('change', filtrar)
+document.getElementById('reaccion').addEventListener('change', filtrar)
+
+function filtrar() {
+    const tipo = document.getElementById('tipo').value;
+    const reaccion = document.getElementById('reaccion').value;
+
+    let equiposFiltrados = [];
+    for (let i = 0; i < todosEquipos.length; i++) {
+        const cumpleTipo = tipo === "default" || todosEquipos[i].tipo === tipo
+        const cumpleReaccion = reaccion === "default" || todosEquipos[i].reaccion === reaccion
+
+        if (cumpleTipo && cumpleReaccion) {
+            equiposFiltrados.push(todosEquipos[i])
+        }
+    }
+
+    let filas =""
+    for (let i = 0; i < equiposFiltrados.length; i++) {
+        filas += "<tr><td>" +
+        equiposFiltrados[i].nombre +
+        "</td><td>"+
+        equiposFiltrados[i].tipo+
+        "</td><td>"+
+        equiposFiltrados[i].reaccion+
+        "</td><td>"+
+        equiposFiltrados[i].personajes+
+        "</td><td>"+
+        equiposFiltrados[i].descripcion+
+        "</td></tr>"
+    }
+    tabla.innerHTML = filas
 }
